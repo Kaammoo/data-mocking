c9ab602175fa940b9d5560c392f5b779c03ab8dd
Kamo <kamo.harutyunyan.ec@gmail.com>
2024-03-13 22:25:24 +0400

    Added scripts for fields and fields_communities tables

--- script/db.py
+++ script/db.py
@@ -1,18 +1,9 @@
 import psycopg2
 
 con = psycopg2.connect(
-database="data_mocking",
-user="my_user",
+database="smart_farm",
+user="farm",
 password="123",
 host="localhost",
 port= '5432'
 )
-
-cursor_obj = con.cursor()
-cursor_obj.execute("SELECT * FROM product")
-result = cursor_obj.fetchall()
-print(result[-1])
-
-cursor_obj.execute("SELECT * FROM users")
-result = cursor_obj.fetchall()
-print(result[-1])
