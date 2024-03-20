58bd92ff89ae6f8e3e986ee736b9fe36017a5657
Kamo <kamo.harutyunyan.ec@gmail.com>
2024-03-20 21:36:04 +0400

    Added inser_plantings function

--- script/main.py
+++ script/main.py
@@ -1,10 +1,10 @@
 from utility import DataMocking
 def run():
     project = DataMocking()
-    #project.insert_fields()
-    #project.insert_product_type()
-    #project.insert_products()
-    #project.insert_portable_devices()
-    #project.insert_records()
+    project.insert_fields()
+    project.insert_precipitation_types()
+    project.insert_products()
+    project.insert_portable_devices()
+    project.insert_records()
     project.insert_plantings()
 run()
--- script/utility.py
+++ script/utility.py
@@ -9,21 +9,28 @@ class DataMocking:
         self.cursor_obj = con.cursor()
 
 
-    def insert_product_type(self):
-        self.cursor_obj.execute("SELECT id FROM products")
-        product_type_data = (
-            ("fruit",),
-            ("vegetables",),
-            ("cereal",),
-            ("berry",),
+    def insert_precipitation_types(self):
+        precipitation_types = (
+            ("rain", ),
+            ("snow",),
+            ("hail",)
         )
-        for product_type in product_type_data:
-            self.cursor_obj.execute("INSERT INTO product_types (type) VALUES (%s)", product_type)
+        for prec_type in precipitation_types:
+            self.cursor_obj.execute("INSERT INTO prec_types (name) VALUES (%s)", prec_type)
 
 
     def insert_products(self):
+        self.cursor_obj.execute("SELECT id FROM product_types WHERE type = 'vegetables'")
+        vegetable_id = self.cursor_obj.fetchone()
+        self.cursor_obj.execute("SELECT id FROM product_types WHERE type = 'cereal'")
+        cereal_id = self.cursor_obj.fetchone()
         for product in products_armenia:
-            self.cursor_obj.execute("INSERT INTO products (name, description) VALUES (%s, %s)", product[:2])
+            if "vegetable" in product[1].lower():
+                type_id = vegetable_id
+            else:
+                type_id = cereal_id
+            self.cursor_obj.execute("INSERT INTO products (type_id, name, description) VALUES (%s, %s, %s)",\
+                (type_id, product[0], product[1]))
 
 
     def insert_fields(self):
@@ -58,14 +65,15 @@ class DataMocking:
             self.cursor_obj.execute(f"SELECT * FROM fields WHERE name LIKE '{community_name}'")
             fields = self.cursor_obj.fetchall()
             for field in fields:
-                self.cursor_obj.execute("INSERT INTO records (community_id, field_id, product_id) VALUES (%s, %s, %s)",\
-                                        (community_id, field[0], random.choice(products)[0]))
+                unique_products = random.sample(products, 5)
+                for product in unique_products:
+                    self.cursor_obj.execute("INSERT INTO records (community_id, field_id, product_id) VALUES (%s, %s, %s)", \
+                                        (community_id, field[0], product[0]))
 
 
     def insert_portable_devices(self):
         for device_name in portable_device_data:
             self.cursor_obj.execute("INSERT INTO portable_devices (name) VALUES (%s)", (device_name,))
-
 
 
     def get_crops_and_months_by_product_name(self, products, product_name):
@@ -89,20 +97,20 @@ class DataMocking:
         years_duration = tuple((i for i in range(1, duration + 1)))
         for record in records:
             field_id = record[2]
-            if field_id in used_records:
+            if field_id in used_records.keys():
                 if len(used_records[field_id]) >= duration:
                     continue
                 elif len(used_records[field_id]) == 1:
-                    years_range = (x for x in years_duration if x != used_records[field_id])
+                    years_range = tuple(x for x in years_duration if x != used_records[field_id][0])
                     rand_year = random.choice(years_range)
-                    used_records[field_id] += (rand_year,)
+                    used_records[field_id].append(rand_year)
                 else:
                     years_range = tuple(item for item in years_duration if item not in used_records[field_id])
                     rand_year = random.choice(years_range)
-                    used_records[field_id].add(rand_year)
+                    used_records[field_id].append(rand_year)
             else:
                 rand_year = random.choice(years_duration)
-                used_records[field_id] = (rand_year,)
+                used_records[field_id] = [rand_year]
             self.cursor_obj.execute(f"SELECT name FROM products WHERE id = {record[1]}")
             product_name = self.cursor_obj.fetchone()[0]
             crop_info = self.get_crops_and_months_by_product_name(products_armenia, product_name)
@@ -120,12 +128,9 @@ class DataMocking:
                 workers_count += random.randint(0, 2)
             crop_count = (field_size * random.randint(min_crop_count, max_crop_count)) / 1000
 
-            self.cursor_obj.execute("INSERT INTO plantings (record_id, crop_quantity, date, workers_quantity) VALUES (%s, %s, %s, %s)",\
-                (record_id, crop_count, random_date_generated, workers_count))
+            self.cursor_obj.execute("INSERT INTO plantings (record_id, crop_quantity, date, workers_quantity) VALUES (%s, %s, %s, %s)",
+                                    (record_id, crop_count, random_date_generated, workers_count))
 
-        
         con.commit()
         self.cursor_obj.close()
         con.close()
-
-
